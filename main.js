8e697a91538af88f4cc52f5cb2b8dde0bbc2877d
Batman@001 <santrasuraj14@gmail.com>
2024-06-09 00:56:07 +0530

    update the files

--- main.js
+++ main.js
@@ -4,12 +4,11 @@ let input = document.querySelector(".input-box");
 
 for (let button of buttons){
     button.addEventListener("click", (e) => {
-        
-        
-            input.value = input.value.concat(button.textContent);
-        
-        
-        
+        button.style.backgroundColor = "rgb(228, 215, 215)";
+        setTimeout(() => {
+            button.style.backgroundColor = "rgb(129, 125, 125)";
+        }, 50);
+        input.value = input.value.concat(button.textContent);
     });
 }
 
@@ -19,8 +18,12 @@ let clear = document.querySelector(".buttons .clear");
 
 equals.addEventListener("click",(e) => {
     try{
-        input.value.replaceAll("÷","/");
-        input.value.replaceAll("×","*");
+        equals.style.backgroundColor = "rgb(228, 215, 215)";
+        setTimeout(() => {
+            equals.style.backgroundColor = "rgb(255, 123, 0)";
+        }, 50);
+        input.value = input.value.replaceAll("÷","/");
+        input.value = input.value.replaceAll("×","*");
         input.value = eval(input.value);
     }
     catch(error){
@@ -29,9 +32,44 @@ equals.addEventListener("click",(e) => {
 });
 
 allclear.addEventListener("click",(e) => {
+    allclear.style.backgroundColor = "rgb(228, 215, 215)";
+        setTimeout(() => {
+            allclear.style.backgroundColor = "rgb(129, 125, 125)";
+        }, 50);
     input.value = "" ;
 });
 
 clear.addEventListener("click",(e) => {
+    clear.style.backgroundColor = "rgb(228, 215, 215)";
+        setTimeout(() => {
+            clear.style.backgroundColor = "rgb(129, 125, 125)";
+        }, 50);
+    input.value = input.value.slice(0,input.value.length - 1);
+});
+
+// for mobile version
+
+for (let button of buttons){
+    button.addEventListener("touchstart", (e) => {
+        input.value = input.value.concat(button.textContent);
+    });
+}
+
+equals.addEventListener("touchstart",(e) => {
+    try{
+        input.value = input.value.replaceAll("÷","/");
+        input.value = input.value.replaceAll("×","*");
+        input.value = eval(input.value);
+    }
+    catch(error){
+        input.value = "" ;
+    }
+});
+
+allclear.addEventListener("touchstart",(e) => {
+    input.value = "" ;
+});
+
+clear.addEventListener("touchstart",(e) => {
     input.value = input.value.slice(0,input.value.length - 1);
 });
